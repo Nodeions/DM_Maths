55f15ffe7496453f26f1719ef4e74bf7fae7b351
Nodeions <59334371+Nodeions@users.noreply.github.com>
2024-04-16 14:56:09 +0200

    start_version

---
+++ exo1.py
@@ -0,0 +1,44 @@
+import math
+
+matrice = [[1,0,2],[0,0,0],[1,0,3]]
+u = [1,2,1]
+v = [2,1,1]
+
+class function:
+    def __init__(self):
+        pass
+
+    def scal(self,u,v):
+        return u[0]*v[0]+u[1]*v[1]+u[2]*v[2]
+    
+    def norme(self, u):
+        return math.sqrt((u[0]**2 + u[1]**2 + u[2]**2))
+    
+    def det(self, matrice):
+
+        deltaOneOne = matrice[1][1]*matrice[2][2]-matrice[2][1]*matrice[1][2]
+        deltaTwoOne = matrice[0][1]*matrice[2][2]-matrice[2][1]*matrice[0][2]
+        deltaThreeOne = matrice[0][1]*matrice[1][2]-matrice[1][1]*matrice[0][2]
+
+        
+        return deltaOneOne+deltaTwoOne+deltaThreeOne
+    
+    #def transformationIdentite(self):
+
+def checkTransfo():
+    if function.det != 1 or function.det != -1:
+        print("Pas isométrique")
+    else:
+        if matrice[0][0] == 1 and matrice[1][1] == 1 and matrice[2][2] == 1:
+            print("Matrice identité")
+        else:
+            #pas fini car pas sûr d'où je vais'
+            return 0
+
+
+def main():
+    print(function.scal(function,u,v))
+    print(function.norme(function,u))
+    print(function.det(function,matrice))
+
+main()
